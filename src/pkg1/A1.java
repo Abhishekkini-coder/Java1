02898ce9c79ab9c3a4a5637ddc19a0fa699bb1d7
Abhishek <abhishekkini.sangamone@gmail.com>
2025-01-29 21:30:14 +0530

    Incomplete Word building game

--- src/pkg1/A1.java
+++ src/pkg1/A1.java
@@ -2,19 +2,25 @@ package pkg1;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-
+import java.util.List;
+import java.util.ArrayList;
 public class A1 {
 	
 
 	public static void main(String[] args) throws FileNotFoundException {
 		A1 a1 = new A1();
+		String line1 = "";
+		List<String>list1 = new ArrayList<>();
 		File f1= new File("dictionary.txt");
 		Scanner sc2 = new Scanner(f1);
 		Scanner sc1 = new Scanner(System.in);
+		while(sc2.hasNext()) {
+			line1 = sc2.nextLine();
+			list1.add(line1);
+		}
+	  
 		
-		System.out.println(sc2.nextLine());
-		System.out.println(sc2.nextLine());
-		System.out.println(sc2.nextLine());
+		System.out.println(list1);
 		
 		
 
